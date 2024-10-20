78f184facfe2e7dd6b094fe53fc44a30a3d1a661
sunrisesineast <raiyanislam247@gmail.com>
2024-10-20 15:15:05 +0600

    fix bug

--- background.js
+++ background.js
@@ -1,12 +1,14 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.action === "openEditor") {
+        const screenWidth = message.screenWidth;
+        const screenHeight = message.screenHeight;
         chrome.windows.create({
             url: "editor.html",
             type: "popup",
             width: 600,
             height: 400, 
-            left: screen.width / 2 - 300, 
-            top: screen.height / 2 - 200
+            left: screenWidth / 2 - 300, 
+            top: screenHeight / 2 - 200
         }, function (popup) {
             chrome.storage.local.set({ text: message.text });
         });
--- content.js
+++ content.js
@@ -1,6 +1,14 @@
 document.addEventListener('focusin', function (event) {
     if (event.target.tagName === 'TEXTAREA' || event.target.tagName === 'INPUT' && event.target.type === 'text') {
-        chrome.runtime.sendMessage({ action: "openEditor", text: event.target.value });
+        const screenWidth = screen.width;
+        const screenHeight = screen.height;
+        chrome.runtime.sendMessage({ 
+            action: "openEditor", 
+            text: event.target.value,
+            screenWidth: screenWidth,
+            screenHeight: screenHeight,
+            
+        });
         event.target.blur();
     }
 })
