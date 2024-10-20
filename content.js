123fd5d8d4281b79afa0d6141cd6309aaf345b73
sunrisesineast <raiyanislam247@gmail.com>
2024-10-20 15:45:31 +0600

    CSP allowance

--- content.js
+++ content.js
@@ -1,7 +1,7 @@
 document.addEventListener('focusin', function (event) {
     if (event.target.tagName === 'TEXTAREA' || event.target.tagName === 'INPUT' && event.target.type === 'text') {
-        const screenWidth = screen.width;
-        const screenHeight = screen.height;
+        const screenWidth = window.screen.width;
+        const screenHeight = window.screen.height;
         chrome.runtime.sendMessage({ 
             action: "openEditor", 
             text: event.target.value,
